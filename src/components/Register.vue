31fd393cba1aaca45b34f54eb72aee04a8a99f91
Kovszasz <kovszasz@gmail.com>
2020-04-13 20:14:29 +0200

    paypal

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -175,12 +175,18 @@
        >
         Next
       </v-btn>
+      <div id="paypal-button-container"></div>
     </v-card-actions>
   </v-card>
 </template>
 <script>
 import api from '../api.js'
 import axios from 'axios'
+import LoadScript from 'vue-plugin-load-script'
+import Vue from 'vue'
+Vue.use(LoadScript)
+Vue.loadScript('https://www.paypal.com/sdk/js?client-id=AeZVhpLFruz2FcxjS3pCXiRapcEOBj8jtb0PuO-ISS_-EPb9q3cYcEZ1R3VIZl4BlnNelbo7yBMJmwnm')
+
 export default {
   name: 'Register',
   props: {
@@ -209,6 +215,28 @@ export default {
         default: return 'Account created'
       }
     }
+  },
+  mounted() {
+    paypal.Buttons({
+      createOrder: function(data, actions) {
+        // This function sets up the details of the transaction, including the amount and line item details.
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: '0.01'
+            }
+          }]
+        });
+      },
+      onApprove: function(data, actions) {
+        // This function captures the funds from the transaction.
+        return actions.order.capture().then(function(details) {
+        // This function shows a transaction success message to your buyer.
+          alert('Transaction completed by ' + details.payer.name.given_name);
+        });
+      }
+  }).render('#paypal-button-container');
+  // This function displays Smart Payment Buttons on your web page.
   },
   methods: {
     send_order () {
